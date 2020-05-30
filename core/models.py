c9402737019298efda628913c82caeaa2be74131
Lev Maximov <lev.maximov@gmail.com>
2020-05-31 02:13:52 +0700

    Book and Comment models defined

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    author = GenericForeignKey()
+    
