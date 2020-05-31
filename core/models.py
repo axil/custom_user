03c84266e0bb8575f878e2980372a96d3ecfd4c3
Lev Maximov <lev.maximov@gmail.com>
2020-05-31 19:34:46 +0700

    custom user: migration

--- core/models.py
+++ core/models.py
@@ -1,8 +1,14 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 from django.db import models
+
+import sys
+if sys.argv[1] != 'migrate':
+    class User(AbstractUser):
+        class Meta:
+            db_table = 'auth_user'
 
 class Book(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- custom_user/settings.py
+++ custom_user/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+import sys
+if sys.argv[1] != 'migrate':
+    AUTH_USER_MODEL = 'core.User'
