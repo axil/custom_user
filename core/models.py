c6ea00779e116d7ff9c9520d8b0399bc492ac388
Lev Maximov <lev.maximov@gmail.com>
2020-05-31 18:04:09 +0700

    preparation

--- core/models.py
+++ core/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
 from django.db import models
 
 class Book(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
