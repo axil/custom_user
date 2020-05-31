b96e5859f9f6193ff74f4c65563a0cc87760deef
Lev Maximov <lev.maximov@gmail.com>
2020-05-31 20:35:03 +0700

    admin.py and createsuperuser admin:admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+@register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'date_of_birth')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.db import models
 
 class User(AbstractUser):
-    birthday = models.DateTimeField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
 
     class Meta:
         db_table = 'auth_user'
@@ -17,4 +17,3 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     author = GenericForeignKey()
-    
